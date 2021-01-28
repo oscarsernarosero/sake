784b87d736ec0a853bee2b3017b7ddb1a4686c47
Oscar Serna <oscar.eduardo.serna@gmail.com>
2021-01-27 20:36:52 -0600

    lending pool started

--- LendingPool.sol
+++ LendingPool.sol
@@ -1,27 +1,52 @@
 pragma solidity ^0.8.0;
 
-contract LendingPool {
+
+//safemath?
+
+contract Owned {
+    
+    address public owner;
+
+    function isOwned() internal {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+}
+
+contract LendingPool is Owned {
+    
+    address[] public lenders;
     
     uint public immutable maxSize;
+    mapping( address => uint ) public balances;
+    
+    //mapping (address => uint) pendingWithdrawals;
     
     constructor(uint _maxSize){
+        
         maxSize = _maxSize;
+        
+        isOwned();
     }
     
-    mapping( address => uint ) balances;
     
     receive() external payable{
         
         require(address(this).balance + msg.value <= maxSize, "This pool is full");
         balances[msg.sender] += msg.value;
+        lenders.push(msg.sender);
         
     }
     
-    function calculateInterestsYield(address lender) private {
+    function calculateInterestsYield(address lender) public {
         
     }
     
-    function liquidateLoan(address loanAddr) private {
+    function liquidateLoan(address loanAddr) public {
         
     }
     
@@ -29,6 +54,18 @@ contract LendingPool {
         
     }
     
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
     
+    function testOnlyLiquidatePool() external onlyOwner returns (bool success){
+        payable(owner).transfer(address(this).balance);
+        
+        uint64 i=0;
+        for (i; i<lenders.length; i++) {
+            balances[lenders[i]] = 0; 
+            }
+        return true;
+    }
     
 }
