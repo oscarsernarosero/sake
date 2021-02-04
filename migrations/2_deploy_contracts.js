8e72d7613a196a58e6e567818f84083f4ace29ad
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2021-02-03 19:56:21 -0600

    Add migrations/ directory to deploy from Truffle

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,13 @@
+var CreditToken = artifacts.require("CreditToken.sol"); // First
+var LendingPool = artifacts.require("LendingPool.sol"); // Second
+require("dotenv").config({path: "../.env"});
+var BN = web3.utils.BN;
+
+// Function to deploy
+module.exports = async function(deployer) {
+    let addresses = await web3.eth.getAccounts();
+
+    await deployer.deploy(CreditToken);
+
+    await deployer.deploy(LendingPool, 100);
+}
